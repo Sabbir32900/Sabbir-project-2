8ef6300414166162720127e724d969e13b1612b5
Sabbir Hossain <137594833+Sabbir32900@users.noreply.github.com>
2023-06-27 16:33:01 +0600

    first commit

---
+++ hallo.js
@@ -0,0 +1,71 @@
+
+//var sabbir = 67447884;
+//console.log(sabbir)
+//var sabbir = "gyr ghdr sgggrys hrsf bth a6t tudfrev t6fegh";
+//console.log(sabbir)
+//var booltype = false ;
+//console.log(booltype)
+
+//var rocona ="THE PROBLEM IS GETTING WORSE DAY BY DAY. THERE ARE MANY CAUSES OF LOAD SHEDDING. MISUSE, ILLEGAL CONNECTION AND INSUFFICIENT PRODUCTION ARE THE MAIN CAUSES OF LOAD SHEDDING. SOME DISHONEST PERSONS ARE ALSO RESPONSIBLE FOR IT";
+
+//console.log(rocona.toString());
+//var myfd = "sabbir munna nurhossain mahedi abdulla";
+//console.log(myfd.indexOf("d"));
+// var nam1 = 390.5553;
+// var nam2 =490;
+// nam1 ="" + nam1;
+// var mot = nam1 + nam2 ;
+// console.log(mot);
+// var nam2 = 550;
+// var nam3 = 4;
+// var mot = nam2 / nam3;
+// console.log(mot);
+// var num1 = 20;
+// var votar = 99.4999;
+
+// var sum = Math.random(votar) * 100;
+// console.log(sum);
+//  var num1 = 10;
+//  var num2 = 20;
+// if(num1 < num2){    
+//     console.log("ami na go tumi");
+// }
+//   else {
+//     console.log("sabbir amr name na yes ha ami na");   }
+// var x = 50;
+// var y = 50;
+// if (x > y){
+//     console.log("x boro");
+// }
+// else if(x < y){
+//     console.log("y boro");
+// }
+
+// else{
+//     console.log("same");
+// }
+// var sum =10;
+// switch(sum){
+//     case 10:
+//         console.log("ami jabo na tumi jao abogh ano ok");
+//         break;
+//     case 5:
+//         console.log("go ami jabo na tumi jao");
+//         break;
+//     case 1:
+//         console.log("ha ha ami na go");
+//         break;
+//     default:
+//         console.log("ai tahole ami jaba ni ok");
+// }
+// var mydate = new Date();
+// console.log(mydate.toLocaleString());
+
+var fdname = ["sabbir" , "munna" , "mahedi"];
+fdname[1] = 50;
+fdname.push("nurhossain");
+fdname.pop();
+fdname.unshift("nurhossain");
+fdname.shift();
+
+console.log(fdname);
