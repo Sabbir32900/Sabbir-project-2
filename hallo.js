0194b808951c3043714ef392ed4bcab5cef0226a
Sabbir Hossain <137594833+Sabbir32900@users.noreply.github.com>
2023-06-29 11:09:34 +0600

    first commit

--- hallo.js
+++ hallo.js
@@ -61,11 +61,122 @@
 // var mydate = new Date();
 // console.log(mydate.toLocaleString());
 
-var fdname = ["sabbir" , "munna" , "mahedi"];
-fdname[1] = 50;
-fdname.push("nurhossain");
-fdname.pop();
-fdname.unshift("nurhossain");
-fdname.shift();
+// var fdname = ["sabbir" , "munna" , "mahedi"];
+// fdname[1] = 50;
+// fdname.push("nurhossain");
+// fdname.pop();
+// fdname.unshift("nurhossain");
+// fdname.shift();
 
-console.log(fdname);
+// console.log(fdname);
+
+
+// var x = 20;
+// var y =20;
+// if(x < y){
+//     console.log("y boro");
+
+// }
+// else if(x > y){
+//     console.log("x boro");
+// }
+// else {
+//     console.log("x and y 2 ta same")
+// }
+
+
+// var sum = "sabbir";
+// switch(sum){
+//     case 1:
+//         console.log("namebar 1 soathik");
+//         break;
+//     case 5:
+//         console.log("namebar 5 soathik");
+//         break;
+//     case "sabbir":
+//         console.log("namebar sabbir soathik")
+//         break;
+//         default:
+//             console.log("ar na hole sob pagol");
+// }
+
+
+// var sabbirdate = new Date;
+// console.log(sabbirdate.toLocaleString());
+
+
+// var sabbir = new Date;
+// console.log(sabbir.toLocaleString())
+
+
+
+// var fdname = ["sabbir" , "munna" , "mahedi" , "nurhossain"];
+// console.log(fdname.slice(1,3));
+
+
+
+// var fdname = ["sabbir" , "munna" , "mahedi" , "nurhossain"];
+// fdname.splice(0,2, "sabbir hossain" , "munna hossain" , "mahedi hossain");
+
+// console.log(fdname);
+
+
+
+// for(var i = 0; i <= 5; i++){
+//     console.log(i);
+// }
+
+// var sum = [0,1,2,3,4,5,6,7,8,9];
+// for(var i = 0; i <= sum.length; i++){
+//     console.log(i);
+// }
+
+// var i = 0;
+// while(i < 5 ){
+//     console.log("sabbir hallo word");
+//     i++
+
+
+// }
+
+
+// function sum (nam1,nam2){
+//     var total = nam1 * nam2;
+//     console.log(total);
+
+// }
+
+// sum(10,10);
+// sum(5,5);
+
+//function sum (nam1){
+//     var total = nam1 * 10;
+//     return total;
+
+// }
+// var a = sum(10);
+// var b = sum(20);
+// console.log(a / b);
+
+
+function Myname (YourName,myparens,phone){
+    this.YourName = YourName;
+    this.myparens = myparens;
+    this.phone    = phone   ;
+    this.sum      = function(){
+        console.log(this.YourName);
+        console.log(this.myparens);
+        console.log(this.phone);
+    }
+
+}
+
+var newobj = new Myname("sabbir hossain" , "furoja khatun" , 01319458837);
+var newobj = new Myname("niloy hossain" , "furoja khatun" , 01767422899);
+var newobj = new Myname("mahedi hossain" , "furoja khatun" , 01314544855);
+var newobj = new Myname("munna hossain" , "furoja khatun" , 01313844056);
+
+newobj.sum();
+
+
+
